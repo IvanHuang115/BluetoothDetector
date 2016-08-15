88072f3cccd25b041ebc545166483cb426e13620
IvanHuang <ivanhuang115@gmail.com>
2016-08-15 13:23:14 -0700

    Users can now input their names and RPi IP address to the app; currently has an issue when reopening app, it may not detect beacons

--- app/src/main/java/com/example/acer/bluetoothdetector/MainActivity.java
+++ app/src/main/java/com/example/acer/bluetoothdetector/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.acer.bluetoothdetector;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
@@ -14,7 +20,6 @@ import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.service.RunningAverageRssiFilter;
 
-//TODO not sure if supposed to import from java.sql, or mysql.jdbc
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -33,28 +38,33 @@ import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
+    private EditText ET_NAME;
+    private EditText ET_IP;
+    private EditText ET_PORT;
+
     private static final String TAG = "Beacon Test";
     private BeaconManager beacMan;
 
     // All beacons have been set to this UUID
     private static final String REGION_UUID = "A580C8B8-89FE-4548-8A24-472B7DE1224C";
-    // Beacon 49893, Major: 0, Minor: 49893
-    // Beacon 49994, Major: 0, Minor: 49994
-    // Beacon 50179, Major: 0, Minor: 50179
-    // Beacon 50337, Major: 0, Minor: 50337
+    // Beacon 49893, Major: 0, Minor: 49893 ==> Beacon1
+    // Beacon 49994, Major: 0, Minor: 49994 ==> Beacon2
+    // Beacon 50179, Major: 0, Minor: 50179 ==> Beacon3
+    // Beacon 50337, Major: 0, Minor: 50337 ==> Beacon4
 
-    private static final String DB_IP = "172.20.10.3";
-    private static final String DB_PORT = "3306";
+    private static String P_ID;
+    private static String P_USER;
+    private static String DB_IP = "137.110.97.150";
+    private static String DB_PORT = "3306";
     private static final String DB_NAME = "app_test";
     private static final String DB_USER = "Ivan";
     private static final String DB_PASS = "Ivan";
     private static final String DB_TABLE = "test_data";
-    private static final String COL_TIME = "ID";
-    private static final String COL_NAME = "NAME";
-    private static final String COL_BEAC1 = "";
-    private static final String COL_BEAC2 = "";
-    private static final String COL_BEAC3 = "";
-    private static final String COL_BEAC4 = "";
+    private static final String COL_PID = "PID";
+    private static final String COL_MONTH = "MONTH";
+    private static final String COL_DAY = "DAY";
+    private static final String COL_MS = "MS";
+    private static final String COL_BEAC = "BEACON";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,44 +72,40 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /*
-        try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-        } catch (Exception e) {
-            Log.d(TAG, "Error finding new instance of the driver class");
-            e.printStackTrace();
-        }
-
-        try {
-            String db = "jdbc:mysql://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
-            Connection connection = DriverManager.getConnection(db, DB_USER, DB_PASS);
-            Log.d(TAG, "got connection");
-
-            connection.prepareStatement(db);
-            Statement statement = connection.createStatement();
-            Log.d(TAG, "created statement");
-
-            String query = "INSERT INTO " + DB_TABLE + " VALUES(2, 'Hello');";
-            Log.d(TAG, query);
-            ResultSet result = statement.executeQuery(query);
-            Log.d(TAG, "query should be sent to db");
-
-        } catch (Exception e) {
-            Log.d(TAG, "Error in getting connection or sending query");
-            e.printStackTrace();
-        }
-        */
-
-
+        TelephonyManager tManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
+        P_ID = tManager.getDeviceId();
+        Log.d(TAG, "device id: " + P_ID);
 
         // Changes the delay for calculating distance from the default of 20 sec to 2.5 sec
         BeaconManager.setRssiFilterImplClass(RunningAverageRssiFilter.class);
         RunningAverageRssiFilter.setSampleExpirationMilliseconds(2500l);
 
         beacMan = BeaconManager.getInstanceForApplication(this);
-
         // Do not have to call beacMan.getBeaconParsers().add() if we stick to AltBeacon protocol
-        beacMan.bind(this);
+
+        ET_NAME = (EditText) findViewById(R.id.ET_NAME);
+        ET_IP = (EditText) findViewById(R.id.ET_IP);
+        ET_PORT = (EditText) findViewById(R.id.ET_PORT);
+
+        SharedPreferences sp = getSharedPreferences("BluetoothDetectorData", Context.MODE_PRIVATE);
+        P_USER = sp.getString("SP_USER", "");
+        DB_IP = sp.getString("SP_IP", "");
+        DB_PORT = sp.getString("SP_PORT", "");
+
+        Log.d(TAG, "user: " + P_USER + " ip: " + DB_IP + " port: " + DB_PORT);
+
+        if (P_USER.equals("") || DB_IP.equals("") || DB_PORT.equals("")) {
+            Toast.makeText(this, "Please enter your name and the IP address of your Raspberry Pi",
+                    Toast.LENGTH_LONG).show();
+        } else {
+            beacMan.bind(this);
+        }
+
+        //beacMan.bind(this);
+
+        ET_NAME.setText(P_USER);
+        ET_IP.setText(DB_IP);
+        ET_PORT.setText(DB_PORT);
     }
 
     @Override
@@ -110,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
 
     @Override
     public void onBeaconServiceConnect() {
-        Log.d(TAG, "Entered onBeaconServiceC0nnect()");
+        Log.d(TAG, "Entered onBeaconServiceConnect()");
 
         // Only detects beacons with a specific UUID
         final Region reg = new Region("RadBeacons", Identifier.parse(REGION_UUID), null, null);
@@ -148,9 +154,13 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
             public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
                 for(Beacon beac : collection) {
                     Calendar now = Calendar.getInstance();
-                    Log.d(TAG, "time (ms)" + now.getTimeInMillis() +
+                    int month = now.get(Calendar.MONTH);
+                    int day = now.get(Calendar.DAY_OF_MONTH);
+                    long ms = now.getTimeInMillis();
+                    Log.d(TAG, "time (ms)" + ms +
                             " distance: " + beac.getDistance() + " UUID:" + beac.getId1() +
-                            " Major:" + beac.getId2() + " Minor:" + beac.getId3());
+                            " Major:" + beac.getId2() + " Minor:" + beac.getId3() +
+                            " RSSI:" + beac.getRssi());
 
                     try {
                         Class.forName("com.mysql.jdbc.Driver").newInstance();
@@ -162,25 +172,14 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
                     try {
                         String db = "jdbc:mysql://" + DB_IP + ":" + DB_PORT + "/" + DB_NAME;
                         Connection connection = DriverManager.getConnection(db, DB_USER, DB_PASS);
-                        if (connection == null) Log.d(TAG, "did NOT get connection");
-                        else {
-                            Log.d(TAG, "got connection");
-                            Log.d(TAG, connection.getCatalog());
-                        }
+                        Log.d(TAG, "got connection");
 
-                        /*
-                        connection.prepareStatement(db);
-                        Statement statement = connection.createStatement();
-                        Log.d(TAG, "created statement");
-                        */
-
-                        String query = "INSERT INTO " + DB_TABLE + " (ID, NAME) VALUES(3, 'Hello3');";
-                        //String query = "SELECT * FROM " + DB_TABLE + ";";
+                        String query = "INSERT INTO " + DB_TABLE + " (ID, NAME) VALUES(" + ms + ", 'Hello3');";
                         Log.d(TAG, query);
                         PreparedStatement statement = connection.prepareStatement(query);
                         statement.execute();
                         Log.d(TAG, "query should be sent to db");
-                        //Log.d(TAG, "Printing out result: " + result.getRow());
+                        // TODO look into closing the connection
 
                     } catch (Exception e) {
                         Log.d(TAG, "Error in getting connection or sending query");
@@ -194,7 +193,23 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
         try {
             beacMan.startMonitoringBeaconsInRegion(reg);
         } catch (RemoteException e) {
+            Log.d(TAG, "problem in startMonitoringBeaconsInRegion");
             e.printStackTrace();
         }
+    }
+
+    public void buttonPressed(View v) {
+        //P_USER = ET_NAME.getText().toString();
+        //DB_IP = ET_IP.getText().toString();
+
+        SharedPreferences sp = getSharedPreferences("BluetoothDetectorData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        // TODO Check for blank entry, eliminate white spaces
+        editor.putString("SP_USER", ET_NAME.getText().toString());
+        editor.putString("SP_IP", ET_IP.getText().toString());
+        editor.putString("SP_PORT", ET_PORT.getText().toString());
+        editor.commit();
+
+        beacMan.bind(this);
     }
 }
